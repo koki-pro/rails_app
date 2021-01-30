0271b8cb1c1350d5bb79f79741d5a9de93a47651
koki-pro <koki.banana03@gmail.com>
2021-01-30 20:35:50 +0900

    ユーザー編集機能作成

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7c780e2353d8625caed8b456e357426a830267dd44062461f96e66d705edf8079d6dab841275a1c1ff331bfdb6b6d351de1a5367638f4fdc675d9a528564393c'
