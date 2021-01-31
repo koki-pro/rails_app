8cd3fdfddfd7424b396adcd00c50933c6033b8dc
koki-pro <koki.banana03@gmail.com>
2021-01-31 10:09:29 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'books/index'
-  get 'books/show'
-  get 'books/new'
-  get 'books/edit'
   devise_for :users
   root to: "home#index"
   resources :users
+  resources :books
 end
